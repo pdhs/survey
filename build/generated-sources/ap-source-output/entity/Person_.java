57d9f00e68ddd3982531067ed7ba481043bce7e4
root <root@pdhssp-ML350p.(none)>
2014-09-13 11:42:13 +0530

    Signed-off-by: root <root@pdhssp-ML350p.(none)>

---
+++ build/generated-sources/ap-source-output/entity/Person_.java
@@ -0,0 +1,14 @@
+package entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2014-09-03T11:45:17")
+@StaticMetamodel(Person.class)
+public class Person_ { 
+
+    public static volatile SingularAttribute<Person, String> name;
+    public static volatile SingularAttribute<Person, Long> id;
+
+}
